c2db9e1e61e463079b27125f280a52951d4d8b4a
Omar Hersi <o.hersi@yahoo.com>
2022-02-18 17:14:47 -0500

    added express-rate-limit package and set rate limit to 100 request 5 mins

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const express = require('express');
 const getMatchData = require('./getMatchData');
 const app = express();
 const cors = require('cors');
+const rateLimit = require('express-rate-limit')
+const res = require('express/lib/response');
 app.use(cors());
 
-app.get('/matches', async (req, res) => {
-    const matches = await getMatchData();
-    res.json(matches);
-});
+
+// Rate Limiting
+const limit = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 mins
+    max: 100
+})
+
+app.use(limit)
+app.set('trust proxy', 1)
+
+try {
+    app.get('/matches', async (req, res) => {
+        const matches = await getMatchData();
+        res.json(matches);
+    });
+}
+catch (error) {
+    res.status(500).json({ error })
+}
+
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))
